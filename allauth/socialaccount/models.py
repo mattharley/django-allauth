92b303e66bcbb8747b2b9def7c2320e926d50e6b
Matt Harley <matt@mattharley.com>
2011-09-24 15:49:47 +0800

    changed socialauth model to be more prettier

--- allauth/socialaccount/models.py
+++ allauth/socialaccount/models.py
@@ -3,8 +3,49 @@ from datetime import datetime
 from django.db import models
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+from itertools import chain
+
+class SocialAccountQuery(object):
+    def __init__(self, models=None):
+        self.models = models
+        self.queries = list()
+        for model in models:
+            self.queries.append(model.objects.all())
+        
+    def __iter__(self):
+        return chain(*(self.queries))
+    
+    def filter(self, *args, **kwargs):
+        qs = self._clone()
+        for query in qs.queries:
+            query = query.filter(*args, **kwargs)
+        return qs
+    
+    def _clone(self):
+        qs = SocialAccountQuery(models=list(self.models))
+        qs.queries = list()
+        for query in self.queries:
+            qs.queries.append(query._clone())
+        return qs
+    
+class SocialAccountManager(object):
+    def get_empty_query_set(self):
+        return models.query.EmptyQuerySet()
+
+    def get_query_set(self):
+        return SocialAccountQuery(SocialAccount.__subclasses__())
+    
+    def none(self):
+        return self.get_empty_query_set()
+    
+    def filter(self, *args, **kwargs):
+        return self.get_query_set().filter(*args, **kwargs)
 
 class SocialAccount(models.Model):
+    objects = SocialAccountManager()
+    
     user = models.ForeignKey(User)
     # No social_id here because I want it to be unique
     last_login = models.DateTimeField(default=datetime.now)
@@ -22,7 +63,6 @@ class SocialAccount(models.Model):
 
     def get_provider(self):
         raise NotImplementedError
-
 
     def get_provider_account(self):
         for f in ['twitteraccount', 'openidaccount', 'facebookaccount']:
